84583fbd432712ed13f8188d6afe36c931cdd0ff
KatsuDono <arttdat@gmail.com>
2023-06-18 06:58:20 +0300

    sqlAddRecord implementation

--- sqlLib.sh
+++ sqlLib.sh
@@ -62,6 +62,55 @@ sqlCreateJob() {
 	fi
 }
 
+sqlAddRecord() {
+	checkSqlCreds 1>/dev/null
+	if [ $? -eq 0 ]; then
+		local sqlFunc="addLogRecord"
+		local hexID totalRuns slotNum recordValue recVal
+		local cmdRes cmdResFilter
+		dmsg inform "args: $*"
+		privateVarAssign "${FUNCNAME[0]}" "hexID" "$1"
+		privateVarAssign "${FUNCNAME[0]}" "recordValue" "$2"
+		privateVarAssign "${FUNCNAME[0]}" "recordValue" $(cut -c3- <<< $recordValue |cut -f1 -d=)
+		case "$recordValue" in
+			"ssh-startup-failed") recVal=1 ;;
+			"ssh-power-down-failed") recVal=2 ;;
+			"ipmi-connection-failed") recVal=3 ;;
+			"ipmi-power-up-failed") recVal=4 ;;
+			"ipmi-power-down-failed") recVal=5 ;;
+			"ssh-connection-failed") recVal=6 ;;
+			"ssh-connection-ok") recVal=7 ;;
+			"ssh-startup-ok") recVal=8 ;;
+			"cycle-started") recVal=9 ;;
+			"cycle-ended") recVal=10 ;;
+			"run-started") recVal=11 ;;
+			"run-ended") recVal=12 ;;
+			"run-passed") recVal=13 ;;
+			"run-failed") recVal=14 ;;
+			"sheet-sync-started") recVal=15 ;;
+			"sheet-sync-ended") recVal=16 ;;
+			"onedrive-sync-started") recVal=17 ;;
+			"onedrive-sync-ended") recVal=18 ;;
+			"onedrive-sync-passed") recVal=19 ;;
+			"onedrive-sync-failed") recVal=20 ;;
+			"onedrive-share-failed") recVal=21 ;;
+			"onedrive-share-passed") recVal=22 ;;
+			"server-up") recVal=23 ;;
+			"db-locked") recVal=24 ;;
+			"db-unlocked") recVal=25 ;;
+			"reboot-unsuccessfull") recVal=26 ;;
+			*) except "unknown record value"
+		esac
+		dmsg echo "CMD> mysql -s -s -s -u ${SQL_USER} -p${SQL_PASSWORD} ${DB_NAME} -e \"SELECT $sqlFunc('$hexID', $recVal);\""
+		cmdRes="$(eval "mysql -s -s -s -u ${SQL_USER} -p${SQL_PASSWORD} ${DB_NAME} -e \"SELECT $sqlFunc('$hexID', $recVal);\"" 2>&1)"
+		cmdResFilter="$(echo "$cmdRes" |grep -v 'Using a password on')"
+		dmsg echo -e "Full res:\n$cmdRes"
+		echo -n "$cmdResFilter"
+	else
+		echo -n -99
+	fi
+}
+
 sqlExportViewCSV() {
 	checkSqlCreds 1>/dev/null
 	if [ $? -eq 0 ]; then
